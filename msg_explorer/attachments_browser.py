05b6c2fd60c67096c1505eb538ddbeb389830a23
TheElementalOfDestruction <arceusthe@gmail.com>
2022-05-30 23:03:04 -0700

    Finished attachment browser (for now, anyways)

--- msg_explorer/attachments_browser.py
+++ msg_explorer/attachments_browser.py
@@ -4,6 +4,7 @@ import extract_msg
 from PySide6 import QtCore
 from PySide6 import QtWidgets
 from PySide6.QtCore import Signal, SIGNAL, Slot, SLOT
+from PySide6.QtWidgets import QTableWidgetItem
 
 from .ui.ui_attachments_browser import Ui_AttachmentsBrowser
 
@@ -17,8 +18,25 @@ class AttachmentsBrowser(QtWidgets.QWidget):
 
     @Slot()
     def msgClosed(self):
-        pass
+        self.ui.tableAttachments.clearContents()
 
     @Slot(extract_msg.msg.MSGFile)
     def msgOpened(self, msgFile):
-        pass
+        try:
+            self.ui.tableAttachments.setRowCount(len(msgFile.attachments))
+            for index, att in enumerate(msgFile.attachments):
+                self.ui.tableAttachments.setItem(index, 0, QTableWidgetItem(str(index)))
+                if isinstance(att, extract_msg.Attachment):
+                    self.ui.tableAttachments.setItem(index, 1, QTableWidgetItem("OK"))
+                    self.ui.tableAttachments.setItem(index, 2, QTableWidgetItem(att.shortFilename))
+                    self.ui.tableAttachments.setItem(index, 3, QTableWidgetItem(att.longFilename))
+                    self.ui.tableAttachments.setItem(index, 4, QTableWidgetItem(att.cid))
+                    self.ui.tableAttachments.setItem(index, 5, QTableWidgetItem(str(att.renderingPosition)))
+                elif isinstance(att, extract_msg.attachment.BrokenAttachment):
+                    self.ui.tableAttachments.setItem(index, 1, QTableWidgetItem("Broken"))
+                elif isinstance(att, extract_msg.attachment.UnsupportedAttachment):
+                    self.ui.tableAttachments.setItem(index, 1, QTableWidgetItem("Unsupported"))
+                else:
+                    self.ui.tableAttachments.setItem(index, 1, QTableWidgetItem("Unknown Type"))
+        except AttributeError as e:
+            print(e)
